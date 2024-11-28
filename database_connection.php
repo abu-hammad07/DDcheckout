b0e970b5709a9bf7d6b801af1917e58a0483c61a
Abu Hammad <hammadkamal2003@gmail.com>
2024-11-28 22:40:05 +0500

    localhost changes

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,16 @@
 <?php
+
+// ====== Local (server)
+// $hostname = "localhost";
+// $username = "root";
+// $password = "";  
+// $database = "dd_check_out";
+
+// ===== Live (Server)
 $hostname = "localhost";
-$username = "root";
-$password = "";  
-$database = "dd_check_out";   
+$username = "u930924801_checkout";
+$database = "u930924801_checkout";   
+$password = "BQoP9>Kt";
+
 $con=mysqli_connect($hostname,$username,$password,$database);    
 ?>   
